c0896d7a7a3b0d8d79a58488f4db7181af60dfbf
Brenda Kelly Oliveira Dos Anjos <brenda6078@digitalcollegesul.local>
2024-09-04 20:07:55 -0300

    início dos métodos com ARRAY

---
+++ index.js
@@ -0,0 +1,9 @@
+let listaJogos = ['crash','Lost Planet', 'Acer Combat']
+
+let estrelas = ['betegeuse', 'sirius', 'anilam', 12, {nome: 'Angelo', idade: 12}]
+
+//console.log(listaJogos[2].toLocaleUpperCase());
+
+let pessoas = ['angelo', 'Juan', 'MAtheus']
+
+pessoas.map((item) => console.log(item.toUpperCase()))
